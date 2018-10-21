b8275397bd5b54ed22904724a90a54892c9680cc
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-10-20 21:34:09 -0400

    figureing out how to show table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,18 +7,33 @@ var connection = mysql.createConnection({
     host: "localhost",
   
     // Your port; if not 3306
-    port: 3000,
+    port: 3006,
   
     // Your username
     user: "root",
   
     // Your password
-    password: "",
+    password: "root",
     database: "bamazon_db"
   });
-  
-  var shop = function() {
-      connection.query('SELECT * FROM products', function(err,res) {
-          console.log(res);
-      })
-  }
+
+  connection.connect(function(err) {
+
+    if (err) throw err;
+    console.log("connected as id " + connection.threatId);
+
+    connection.query('SELECT * FROM products', function (err, response) {
+        if(err) {
+            throw err
+        }
+
+        console.log(response)
+    })
+
+    connection.end();
+  });
+
+
+  console.log ("\nWELCOME TO BAMAZON!"+"\n"+"\n"+ "These are the items we have for sale:")
+  console.log ("=========================================")
+
